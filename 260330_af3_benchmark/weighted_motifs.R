77d4c0b0e03b3278c87acf850932b26579cd5a2a
Rita Ann Roessner <roessner@wle-32-95.unil.ch>
2026-04-16 19:44:06 +0200

    add class ii mhcs

--- 260330_af3_benchmark/weighted_motifs.R
+++ 260330_af3_benchmark/weighted_motifs.R
@@ -13,48 +13,65 @@ make_info <- function(gene, input_name = "Input", baseline_name = "Baseline", mo
 
 baseline.model <- MixTCRviz::baseline_HomoSapiens
 
-df = read.csv('test.csv')
+df = read.csv('AF3_class_I_motif.csv')
 models = unique(df$model)
+print(models)
 df_factors = read.csv('AF3_class_I_factor.csv')
 
-iptm_threshold = 0.5
-out_path = 'motifs/medium_conf'
+iptm_threshold = 0
+out_path = 'motifs/all'
 
 df_filtered <- df[df$AF3_iptm_pair_mean > iptm_threshold , ]
 motif <- MixTCRviz(input1=df_filtered, output.path = out_path)
 
-for (model in models){
-  print(model)
-  factor = df_factors[df_factors$model == model, "factor"]
-  str(factor)
+total_countV <- list(TRA = NULL, TRB = NULL)
+total_countJ <- list(TRA = NULL, TRB = NULL)
+
+for (model in models) {
+  factor <- df_factors$factor[df_factors$model == model][1]
   es <- motif$stat[[model]]
   
-  ch <- "TRA"
-  es$countV[[ch]] <- es$countV[[ch]] * factor
-  es$countJ[[ch]] <- es$countJ[[ch]] * factor
-  motif$stat[[model]] <- es
-  View(motif)
+  for (ch in c("TRA", "TRB")) {
+    currentV <- es$countV[[ch]] * factor
+    currentJ <- es$countJ[[ch]] * factor
+    
+    if (is.null(total_countV[[ch]])) {
+      total_countV[[ch]] <- currentV
+    } else {
+      all_names <- union(names(total_countV[[ch]]), names(currentV))
+      tmp1 <- setNames(rep(0, length(all_names)), all_names)
+      tmp2 <- tmp1
+      tmp1[names(total_countV[[ch]])] <- total_countV[[ch]]
+      tmp2[names(currentV)] <- currentV
+      total_countV[[ch]] <- tmp1 + tmp2
+    }
+    
+    if (is.null(total_countJ[[ch]])) {
+      total_countJ[[ch]] <- currentJ
+    } else {
+      all_names <- union(names(total_countJ[[ch]]), names(currentJ))
+      tmp1 <- setNames(rep(0, length(all_names)), all_names)
+      tmp2 <- tmp1
+      tmp1[names(total_countJ[[ch]])] <- total_countJ[[ch]]
+      tmp2[names(currentJ)] <- currentJ
+      total_countJ[[ch]] <- tmp1 + tmp2
+    }
+  }
 }
 
+str(total_countV[["TRA"]])
 
-for (model in models){
-  print(model)
-  factor = df_factors[df_factors$model == model, "factor"]
-  es <- motif$stat[[model]]
-  
-  ch <- "TRA"
-  info <- make_info(paste0(ch, "V"), model=model)
-  pVA <- plotVJ(count.es=es$countV[[ch]], count.rep=baseline.model$countV[[ch]], info = info)
-  info <- make_info(paste0(ch, "J"), model=model)
-  pJA <- plotVJ(count.es=es$countJ[[ch]], count.rep=baseline.model$countJ[[ch]], info = info)
+ch <- "TRA"
+info <- make_info(paste0(ch, "V"))
+pVA <- plotVJ(count.es=total_countV[[ch]], count.rep=baseline.model$countV[[ch]], sd.rep = baseline.model$sdV[[ch]], info = info)
+info <- make_info(paste0(ch, "J"))
+pJA <- plotVJ(count.es=total_countJ[[ch]], count.rep=baseline.model$countJ[[ch]], sd.rep = baseline.model$sdJ[[ch]], info = info)
 
-  ch <- "TRA"
-  info <- make_info(paste0(ch, "V"), model=model)
-  pVB <- plotVJ(count.es=es$countV[[ch]], count.rep=baseline.model$countV[[ch]], info = info)
-  info <- make_info(paste0(ch, "J"), model=model)
-  pJB <- plotVJ(count.es=es$countJ[[ch]], count.rep=baseline.model$countJ[[ch]], info = info)
-  
-  p_all <- ggarrange(pVA, pJA, pVB, pJB, ncol = 4, nrow = 1)
-  print(p_all)
-}
+ch <- "TRB"
+info <- make_info(paste0(ch, "V"), model=model)
+pVB <- plotVJ(count.es=total_countV[[ch]], count.rep=baseline.model$countV[[ch]], sd.rep = baseline.model$sdV[[ch]], info = info)
+info <- make_info(paste0(ch, "J"), model=model)
+pJB <- plotVJ(count.es=total_countJ[[ch]], count.rep=baseline.model$countJ[[ch]], sd.rep = baseline.model$sdJ[[ch]], info = info)
 
+p_all <- ggarrange(pVA, pJA, pVB, pJB, ncol = 4, nrow = 1)
+ggsave("weighted_motif.png", plot = p_all, width = 16, height = 4, dpi = 300)
